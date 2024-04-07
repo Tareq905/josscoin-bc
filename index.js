7edc21b4a26d521a40d7c7fc9b189a56833cd55f
Tareq905 <tareqshah.027@gmail.com>
2024-04-08 04:30:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,164 @@
+const sha256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+
+//creating block
+class Block{
+    constructor(timestamp,transactions,previousHash = ''){
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    mineBLock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Mining done : "+ this.hash);
+    }
+
+    calculateHash(){
+        return sha256(this.timestamp + 
+            JSON.stringify(this.transactions) + 
+            this.previousHash + this.nonce).toString();
+    }
+
+    hasValidTransaction(){
+        for(const tx of this.transactions){
+            if(!tx.isValid()){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//creating transaction
+
+class Transaction{
+    constructor(fromAddress,toAddress,amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount  =  amount;
+    }
+    calculateHash(){
+        return sha256(
+            this.fromAddress + 
+            this.toAddress + 
+            this.amount
+        ).toString();
+    }
+    signTransaction(key){
+        if(key.getPublic('hex') !== this.fromAddress){
+            throw new Error("You do not have access");
+        }
+        const hashTx = this.calculateHash();
+        const signature = key.sign(hashTx, 'base64');
+        this.signature = signature.toDER();
+    }
+    isValid(){
+        if(this.fromAddress === null) true;
+        if(!this.signature || this.signature.length === 0){
+            throw new Error("No signature found");
+        }
+        const key = ec.keyFromPublic(this.fromAddress,'hex');
+        return key.verify(this.calculateHash(), this.signature);
+    }
+}
+
+//creating blockchain
+
+class Blockchain {
+    constructor(){
+        this.chain = [this.generateGenesisBlock()];
+        this.difficulty = 2;
+
+        this.pendingTransactions = [];
+        this.minigReward = 50;
+    }
+
+    generateGenesisBlock(){
+        return new Block("2019-01-01","GENESIS","0000");
+    }
+
+    getLatestBLock(){
+        return this.chain[this.chain.length-1];
+    }
+
+    addTransaction(transaction){
+        if(!transaction.fromAddress || !transaction.toAddress){
+            throw new Error("Cannot process transaction");
+        }
+        if(!transaction.isValid()){
+            throw new Error("Invalid transaction");
+        }
+        if(transaction.amount < 0){
+            throw new Error("Invalid transaction amount");
+        }
+
+       /*if(transaction.amount > this.getBalanceOfAddress(transaction.fromAddress)){
+            throw new Error("Not enough balance");
+        }
+        */
+
+        this.pendingTransactions.push(transaction);
+    }
+
+    minePendingTransactions(minerAddress){
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBLock(this.difficulty);
+        this.chain.push(block);
+        this.pendingTransactions = [
+            new Transaction(null,minerAddress, this.minigReward)
+        ];
+    }
+
+    /*addBlock(newBLock){
+        newBLock.previousHash = this.getLatestBLock().hash;
+        newBLock.mineBLock(this.difficulty);
+        this.chain.push(newBLock);
+    }*/
+
+    isBlockchinValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBLock = this.chain[i];
+            const previousBLock = this.chain[i - 1];
+            if(currentBLock.hash !== currentBLock.calculateHash()){
+                return false;
+            }
+            if(currentBLock.previousHash !== previousBLock.hash){
+                return false;
+            }
+            if(!currentBLock.hasValidTransaction()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+        return balance;
+    }
+}
+
+//const josscoian = new Blockchain();
+
+module.exports = {
+    Block,
+    Transaction,
+    Blockchain
+}
---
+++ test.js
@@ -0,0 +1,46 @@
+const { Block,
+    Transaction,
+    Blockchain
+} = require('./index');
+
+const EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+
+//generate key
+
+//user1
+const key1 = ec.genKeyPair();
+const privatKey1 = key1.getPrivate("hex");
+const walletNumber1 = key1.getPublic("hex");
+
+//user2
+const key2 = ec.genKeyPair();
+const privatKey2 = key2.getPrivate("hex");
+const walletNumber2 = key2.getPublic("hex");
+
+//create instance
+const josscoian = new Blockchain();
+
+//transaction 1
+const tx1 = new Transaction(walletNumber1,walletNumber2,100);
+tx1.signTransaction(key1);
+josscoian.addTransaction(tx1);
+
+//mining1 for transaction 1
+josscoian.minePendingTransactions(walletNumber1);
+console.log(josscoian.getBalanceOfAddress(walletNumber1));
+console.log(josscoian.getBalanceOfAddress(walletNumber2));
+
+//transaction 2
+const tx2 = new Transaction(walletNumber2,walletNumber1,50);
+tx2.signTransaction(key2);
+josscoian.addTransaction(tx2);
+
+//mining2 for transaction 2
+josscoian.minePendingTransactions(walletNumber1);
+console.log(josscoian.getBalanceOfAddress(walletNumber1));
+console.log(josscoian.getBalanceOfAddress(walletNumber2));
+
+console.log(josscoian.isBlockchinValid());
+
+console.log("Developed By Tareq")
